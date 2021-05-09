786e413ca41bd079647b1b50a730c0e5e8df9e40
Moey Misrabi <moeynaise@gmail.com>
2021-05-09 15:57:07 +0300

    MainTheme class addition

---
+++ lib/constant/MainTheme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MainTheme{
+  static Color gradient_color_1 = Color(0xFF78e08f);
+  static Color gradient_color_2 = Color(0xFFb8e994);
+  static ThemeData finalTheme = ThemeData(
+    primaryColor: Color(0xFF78e08f),
+    accentColor: Color(0xFF78e08f),
+    scaffoldBackgroundColor: Colors.white,
+    fontFamily: 'OpenSans',
+  );
+}
