89eb6d265bc3c4cd2cd163d1beac8e46d00e67b6
Moey Misrabi <moeynaise@gmail.com>
2021-05-10 14:38:23 +0300

    Adding font sizes

--- lib/constant/MainTheme.dart
+++ lib/constant/MainTheme.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
 class MainTheme{
+  static double fontXSmall = 12;
+  static double fontSmall = 14;
+  static double fontMedium = 16;
+  static double fontLarge = 18;
+  static double fontXLarge = 20;
   static Color mainColor = Color(0xFF00B9FF);
   static Color secondaryColor = Color(0xFF3DD6EB);
-  static Color heart_color = Color(0xFFff5959);
+  static Color heartColor = Color(0xFFff5959);
   static ThemeData finalTheme = ThemeData(
     primaryColor: mainColor,
     accentColor: secondaryColor,
