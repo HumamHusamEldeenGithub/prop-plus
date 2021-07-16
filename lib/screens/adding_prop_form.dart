6b4cdf92cf19f533463011bdf0ed92031713ff7c
humam.husam.eldeen@gmail.com <humam.husam.eldeen@gmail.com>
2021-07-16 14:46:45 +0300

    Fixing Add property form

--- lib/screens/adding_prop_form.dart
+++ lib/screens/adding_prop_form.dart
@@ -17,56 +17,58 @@ class _PropertyInputFormState extends State<PropertyInputForm> {
   Future<void> sendApprovalRequest() async {
     print(_title);
 
-    var userId = locater.get<UserController>().currentUser.dbId;
+    //TODO : get user's database id and use it here
+    //var userId = locater.get<UserController>().currentUser.uid;
+    var userId = 954;
     print(userId) ;
 
-    // final response = await http.post(
-    //   Uri.parse(
-    //       'https://propplus-production.herokuapp.com/properties_to_approve'),
-    //   headers: <String, String>{
-    //     'Content-Type': 'application/json; charset=UTF-8',
-    //   },
-    //   body: jsonEncode(<String, String>{
-    //     'name': _title,
-    //     'user_id': userId.toString(),
-    //     'phone': _phone,
-    //     'description': _description,
-    //   }),
-    // );
-    //
-    // if (response.statusCode == 201 || response.statusCode == 200) {
-    //   // If the server did return a 201 CREATED response,
-    //   // then parse the JSON.
-    //
-    //   Map<String, dynamic> propToApprove = jsonDecode(response.body);
-    //   dynamic propToApproveId = propToApprove['id'];
-    //   print("DONE");
-    //   print(propToApproveId);
-    //
-    //   //TODO iterate over all images
-    //   // for (image in _imageUrl) {
-    //   //   final imageResponse = await http.post(
-    //   //     Uri.parse(
-    //   //         'https://propplus-production.herokuapp.com/approval_images'),
-    //   //     headers: <String, String>{
-    //   //       'Content-Type': 'application/json; charset=UTF-8',
-    //   //     },
-    //   //     body: jsonEncode(
-    //   //         <String, String>{'property_id': propToApproveId, 'url': image.path}),
-    //   //   );
-    //   //
-    //   //   if (imageResponse.statusCode == 201 ||
-    //   //       response.statusCode == 200) {} else {
-    //   //     throw Exception('Failed to post to  approval_images table  .');
-    //   //   }
-    //   // }
-    //   //TODO : return a flag to show succeeded widget
-    // } else {
-    //   // If the server did not return a 201 CREATED response,
-    //   // then throw an exception.
-    //   print(response.statusCode);
-    //   throw Exception('Failed to post to  properties_to_approve table  .');
-    // }
+    final response = await http.post(
+      Uri.parse(
+          'https://propplus-production.herokuapp.com/properties_to_approve'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'name': _title,
+        'user_id': userId.toString(),
+        'phone': _phone,
+        'description': _description,
+      }),
+    );
+
+    if (response.statusCode == 201 || response.statusCode == 200) {
+      // If the server did return a 201 CREATED response,
+      // then parse the JSON.
+
+      Map<String, dynamic> propToApprove = jsonDecode(response.body);
+      dynamic propToApproveId = propToApprove['id'];
+      print("DONE");
+      print(propToApproveId);
+
+      //TODO iterate over all images
+      // for (image in _imageUrl) {
+      //   final imageResponse = await http.post(
+      //     Uri.parse(
+      //         'https://propplus-production.herokuapp.com/approval_images'),
+      //     headers: <String, String>{
+      //       'Content-Type': 'application/json; charset=UTF-8',
+      //     },
+      //     body: jsonEncode(
+      //         <String, String>{'property_id': propToApproveId, 'url': image.path}),
+      //   );
+      //
+      //   if (imageResponse.statusCode == 201 ||
+      //       response.statusCode == 200) {} else {
+      //     throw Exception('Failed to post to  approval_images table  .');
+      //   }
+      // }
+      //TODO : return a flag to show succeeded widget
+    } else {
+      // If the server did not return a 201 CREATED response,
+      // then throw an exception.
+      print(response.statusCode);
+      throw Exception('Failed to post to  properties_to_approve table  .');
+    }
   }
 
   PropType _propType = PropType.Hotel;
