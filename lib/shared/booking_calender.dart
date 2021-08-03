ffef95bf94542e5f92399315c7aef70c23bd25d4
humam.husam.eldeen@gmail.com <humam.husam.eldeen@gmail.com>
2021-08-03 13:28:23 +0300

    Fixing async in booking_calander

--- lib/shared/booking_calender.dart
+++ lib/shared/booking_calender.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:prop_plus/constant/MainTheme.dart';
 import 'package:prop_plus/modules/booking_module.dart';
@@ -7,67 +9,59 @@ import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 
 class BookingCalender extends StatefulWidget {
   final ServiceModule serviceModule;
-  const BookingCalender({Key key,this.serviceModule}) : super(key: key);
+  const BookingCalender({Key key, this.serviceModule}) : super(key: key);
 
   @override
   _BookingCalenderState createState() => _BookingCalenderState();
 }
 
 class _BookingCalenderState extends State<BookingCalender> {
-
   List<DateTime> blackOutDates = <DateTime>[];
   List<BookingModule> bookingModules;
 
-  Future<List<DateTime>> initailizeBlackOuts() async{
+  Future initailizeBlackOuts() async {
     List<DateTime> curBlackOutDates = <DateTime>[];
     bookingModules = await HTTP_Requests.getAllBookingForService(3.toString());
-    debugPrint(bookingModules.length.toString());
-    for(int i = 0;i < bookingModules.length;i++) {
-      for (int j = 0; j <= bookingModules[i].toDate
-          .difference(bookingModules[i].fromDate)
-          .inDays; j++) {
+    for (int i = 0; i < bookingModules.length; i++) {
+      for (int j = 0;
+          j <=
+              bookingModules[i]
+                  .toDate
+                  .difference(bookingModules[i].fromDate)
+                  .inDays;
+          j++) {
         curBlackOutDates.add(bookingModules[i].fromDate.add(Duration(days: j)));
       }
     }
-    debugPrint(curBlackOutDates.length.toString());
-    for(int i = 0;i < curBlackOutDates.length;i++){
-      debugPrint(curBlackOutDates[i].toString());
-    }
-    return curBlackOutDates;
+    setState(() {
+      blackOutDates=curBlackOutDates;
+    });
   }
 
-
-  void onSelectionChanged(DateRangePickerSelectionChangedArgs args){
-
-  }
+  void onSelectionChanged(DateRangePickerSelectionChangedArgs args) {}
 
   @override
   void initState() {
     super.initState();
-    initailizeBlackOuts().then((value) {
-      setState(() {
-        blackOutDates.addAll(value);
-      });
-    });
+    initailizeBlackOuts();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SfDateRangePicker(
-          monthCellStyle: DateRangePickerMonthCellStyle(
-            cellDecoration: BoxDecoration(
-              color: Colors.white
-            ),
-            blackoutDateTextStyle: TextStyle(
-              color: MainTheme.greyFontColor
-            )
-          ),
-          monthViewSettings: DateRangePickerMonthViewSettings(
-            blackoutDates: blackOutDates,
-          ),
-          onSelectionChanged: onSelectionChanged,
-        )
-    );
+        body: blackOutDates.isNotEmpty
+            ? SfDateRangePicker(
+                monthCellStyle: DateRangePickerMonthCellStyle(
+                    cellDecoration: BoxDecoration(color: Colors.white),
+                    blackoutDateTextStyle:
+                        TextStyle(color: MainTheme.greyFontColor)),
+                monthViewSettings: DateRangePickerMonthViewSettings(
+                  blackoutDates: blackOutDates ,
+                ),
+                onSelectionChanged: onSelectionChanged,
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ));
   }
 }
